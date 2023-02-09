c549d0e741e976f50dc80c6b5dc62b6c88dc280b
Makhmadali-N <124869192+Makhmadali-N@users.noreply.github.com>
2023-02-09 19:13:08 +0500

    Slider HTML CSS JS
    ...

---
+++ app.js
@@ -0,0 +1,19 @@
+function sliderPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll(".slide");
+    slides[activeSlide].classList.add("active");
+    for (const slide of slides) {
+        slide.addEventListener("click", () => {
+            clearActiveClass();
+            slide.classList.add("active");
+        });
+    }
+    function clearActiveClass() {
+        slides.forEach((slide) => {
+            slide.classList.remove("active");
+        }
+
+        )
+    }
+}
+
+sliderPlugin(1);
